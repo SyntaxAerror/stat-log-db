2c8ac08f04b1f31e8388181ab65e326e42a9bee1
bjthres1 <ben.thresher@restyn.com>
2025-09-10 00:11:33 -0400

    fix(log): correct type mapping for log relationships
    Updated the Log model to properly define type_id
    and level_id as integers and added relationships
    for type and level.

--- stat_log_db/src/stat_log_db/modules/log/models/log.py
+++ stat_log_db/src/stat_log_db/modules/log/models/log.py
@@ -1,5 +1,5 @@
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from stat_log_db.modules.base import BaseModel
 from stat_log_db.modules.log import LogType, LogLevel
@@ -8,8 +8,10 @@ from stat_log_db.modules.log import LogType, LogLevel
 class Log(BaseModel):
     __tablename__ = "log"
 
-    type_id: Mapped[LogType] = mapped_column(ForeignKey("log_type.id"), nullable=False)
+    type_id: Mapped[int] = mapped_column(ForeignKey("log_type.id"), nullable=False)
+    type: Mapped[LogType] = relationship()
 
-    level_id: Mapped[LogLevel] = mapped_column(ForeignKey("log_level.id"), nullable=False)
+    level_id: Mapped[int] = mapped_column(ForeignKey("log_level.id"), nullable=False)
+    level: Mapped[LogLevel] = relationship()
 
     message: Mapped[str] = mapped_column(String, nullable=False)
